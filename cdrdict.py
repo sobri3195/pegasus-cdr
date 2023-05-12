c23b0a5219287b4aa4cf364e1ac096001a60abb8
Muhammad Sobri Maulana <sobri3195@gmail.com>
2023-05-12 14:44:48 +0700

    Create cdrdict.py

---
+++ cdrdict.py
@@ -0,0 +1,13 @@
+# Create a CDR dictionary
+cdr = {
+    'caller_number': '+1234567890',
+    'recipient_number': '+9876543210',
+    'call_duration': '5 minutes',
+    'call_timestamp': '2023-05-12 10:15:30'
+}
+
+# Access and display CDR information
+print("Caller Number:", cdr['caller_number'])
+print("Recipient Number:", cdr['recipient_number'])
+print("Call Duration:", cdr['call_duration'])
+print("Call Timestamp:", cdr['call_timestamp'])
